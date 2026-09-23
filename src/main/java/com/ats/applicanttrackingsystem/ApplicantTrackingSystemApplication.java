4364289cd6191fe10227dfb43abd9b6ac8a0c117
XuanToan01-hn <xuantoan10022002@gmail.com>
2026-09-23 13:31:35 +0700

    Init project

---
+++ src/main/java/com/ats/applicanttrackingsystem/ApplicantTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ats.applicanttrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantTrackingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicantTrackingSystemApplication.class, args);
+    }
+
+}
